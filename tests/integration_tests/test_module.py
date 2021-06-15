a7dd41db94bc3cf72eb8c9ff8dc5b8d6da641676
aragong <aragong@unican.es>
2021-06-15 16:05:47 +0200

    fix

--- tests/integration_tests/test_module.py
+++ tests/integration_tests/test_module.py
@@ -5,4 +5,4 @@ from python_module import python_module
 class TestPythonModule(unittest.TestCase):
     def test_python_module(self):
         self.response = python_module.hello_world()
-        self.assertEqual(type(self.response), str)
+        self.assertEqual(type(self.response), dict)
