2821285078657a9328dd6c654dd2d683660d78dc
aragong <aragong@unican.es>
2021-06-15 17:26:01 +0200

    update

--- tests/integration_tests/test_module.py
+++ tests/integration_tests/test_module.py
@@ -6,4 +6,5 @@ from python_module import python_module
 class TestPythonModule(unittest.TestCase):
     def test_python_module(self):
         self.response = python_module.hello_world()
+        self.assertIsNotNone(self.response)
         self.assertEqual(type(self.response), dict)
