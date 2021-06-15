05aed52885234e9e6d48049f3af58fea406daa7c
aragong <aragong@unican.es>
2021-06-15 12:13:01 +0200

    update

---
+++ tests/integration_tests/test_module.py
@@ -0,0 +1,8 @@
+import unittest
+
+from python_module import python_module
+
+class TestPythonModule(unittest.TestCase):
+    def test_python_module(self):
+        self.response = python_module.hello_world()
+        self.assertEqual(type(self.response), dict)
