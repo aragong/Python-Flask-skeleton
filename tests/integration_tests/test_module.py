e2d9ce58c68fc10fac82168b1640002ba2a70815
aragong <aragong@unican.es>
2021-06-15 17:39:47 +0200

    update

--- tests/integration_tests/test_module.py
+++ tests/integration_tests/test_module.py
@@ -4,6 +4,8 @@ from python_module import python_module
 
 
 class TestPythonModule(unittest.TestCase):
+    """Example test"""
+
     def test_python_module(self):
         self.response = python_module.hello_world()
         self.assertIsNotNone(self.response)
